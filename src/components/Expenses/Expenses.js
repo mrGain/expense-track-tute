b5ef8d8aa773ebe1098ad6d622b213d74f391d96
Rakesh Gain <rakeshgain47681@gmail.com>
2023-03-27 10:08:36 +0530

    conditional rendering the list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,20 +12,45 @@ const Expenses = (props) => {
     const filteredExpenses = props.item.filter( expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     })
-    console.log(filteredExpenses)
+    
+    let expenseContent = <p>No Expense found ...!</p>
+    if(filteredExpenses.length > 0){
+        expenseContent = filteredExpenses.map(
+            (expense) =>
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+        )
+    }
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {filteredExpenses.map(
+            {/* {filteredExpenses.length < 1 ? <p>No Expenses found..!</p> :
+                filteredExpenses.map(
+                    (expense) =>
+                        <ExpenseItem
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />
+                    )
+            } */}
+            {/* {filteredExpenses.length < 1 && <p>No Expenses found..!</p>}
+            {filteredExpenses.length > 0 && filteredExpenses.map(
                 (expense) =>
-                    <ExpenseItem
+                    <ExpenseItem   
                         key={expense.id}
                         title={expense.title}
                         amount={expense.amount}
                         date={expense.date}
                     />
                 )
-            }
+            } */}
+            {expenseContent}
         </Card>
 
     );
