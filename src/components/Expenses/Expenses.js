f2fb01e898485f996e90eea093b0d7cda3181229
Rakesh Gain <rakeshgain47681@gmail.com>
2023-03-31 22:55:35 +0530

    Expense Chart added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import './Expenses.css'
 import Card from '../UI/Card';
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseList from "./ExpenseList";
+import ExpensesChart from "./ExpensesChart";
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2023');
     const filterChangeHandler = (selectedYear) => {
@@ -37,6 +38,7 @@ const Expenses = (props) => {
                     />
                 )
             } */}
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpenseList items={filteredExpenses} />
         </Card>
 
